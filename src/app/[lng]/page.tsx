1f6f3087e5ed7f190eace4c1e6888846352138e9
AhmedKhaled6900 <you@example.com>
2024-05-11 23:16:32 +0300

    edit

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -19,47 +19,41 @@ const {t} = await useTranslation(lng ,"third-page")
   return (
   
   <>
- {/* <div className=' mt-10 h-[100%] w-[100%] justify-center text-center items-center grid grid-cols-1 lg:grid-cols-2 '>   
-  {lng==="en"?
-  <div className=' pt-0 lg:pt-10 bg-gradient-to-b w-full h-full from-b to-black lg:rounded-br-[150px]  lg:rounded-tr-[150px] ] '>
-  <h1 className='m-10 lg:m-10 font-semibold text-5xl'> {t('title')}</h1>
-  </div>:<div className=' pt-0 lg:pt-10 bg-gradient-to-b w-full h-full from-b to-black lg:rounded-tl-[150px] lg:rounded-bl-[150px] '>
-  <h1 className='m-10 lg:m-10 font-semibold text-5xl'> {t('title')}</h1>
-  </div> }
-  {
-  lng==="en" ?<div className=' relative w-full h-[100vh] '>
-  <Image className='lg:rounded-tl-[150px] lg:rounded-bl-[150px]' layout="fill" alt=''src={img}/>
-     </div>: <div className=' relative w-full h-[100vh] '>
-<Image className='lg:rounded-tr-[150px] lg:rounded-br-[150px]' layout="fill" alt=''src={img}/>
-   </div>
-  }
-  </div> */}
-  
   <FirstSection lng={lng} ></FirstSection>
-
-
   <Separator/>
-
-
   <SecondSection lng={lng}></SecondSection>
-
- 
-  <div className='w-full h-[500px] relative ' style={{backgroundImage:`${bg}`}}>
+  <div className='w-full h-[500px] relative flex justify-center items-center ' style={{backgroundImage:`${bg}`}}>
     <div>
     <Image src={bg} alt='' fill/>
     </div>
     {
-      lng==="en"?    <div data-aos="fade-right" className=' bg-gradient-to-b p-10 from-b to-black absolute top-20 left-20 overflow-hidden'>
-      <h3 data-aos="fade-right"> {t("first")} </h3>
+      lng==="en"?<div data-aos="fade-right" className=' space-x-4  flex justify-center items-center '>
+        <div className=' bg-gradient-to-b p-5 from-b to-black '>
+        <h3 data-aos="fade-right"> {t("first")} </h3>
       <h3 data-aos="fade-right"> {t("second")} </h3>
       <h3 data-aos="fade-right">{t("third")} </h3>
-    </div>:    <div className='absolute top-20 right-20 overflow-hidden'>
+        </div>
+        <div className=' bg-gradient-to-b p-5 from-b to-black'>
+        <h3 data-aos="fade-right"> {t("first")} </h3>
+      <h3 data-aos="fade-right"> {t("second")} </h3>
+      <h3 data-aos="fade-right">{t("third")} </h3>
+        </div>
+
+    </div>:<div data-aos="fade-right" className='  flex justify-center items-center space-x-4 '>
+      <div className='bg-gradient-to-b p-5 from-b to-black '>
       <h3 data-aos="fade-left"> {t("first")} </h3>
       <h3 data-aos="fade-left"> {t("second")} </h3>
       <h3 data-aos="fade-left">{t("third")} </h3>
+      </div>
+      <div className='bg-gradient-to-b p-5 from-b to-black'>
+      <h3 data-aos="fade-left"> {t("first")} </h3>
+      <h3 data-aos="fade-left"> {t("second")} </h3>
+      <h3 data-aos="fade-left">{t("third")} </h3>
+      </div>
+   
     </div>
     }
-    {
+    {/* {
       lng==="en"?    <div data-aos="fade-right" className=' bg-gradient-to-b p-10 from-b to-black absolute top-60 left-20 overflow-hidden'>
       <h3 data-aos="fade-right"> {t("first")} </h3>
       <h3 data-aos="fade-right"> {t("second")} </h3>
@@ -69,9 +63,7 @@ const {t} = await useTranslation(lng ,"third-page")
       <h3 data-aos="fade-left"> {t("second")} </h3>
       <h3 data-aos="fade-left">{t("third")} </h3>
     </div>
-    }
-
-
+    } */}
   </div>
 </>
   )
